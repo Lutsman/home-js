66e569fe6ff15a09a6044f95fbe670c43b741945
Borysodessa <glimah481gmail.com>
2021-05-21 21:14:26 +0300

    1commit

---
+++ home-js_5.js
@@ -0,0 +1,19 @@
+const destinationCountry = prompt('Введите имя страны');
+let possibleCountry;
+let price;
+const china = 'Китай';
+const australia = 'Австралия'
+const chile = 'Чили';
+const india = 'Индия';
+const jamaica = 'Ямайка';
+let dc;
+dc = destinationCountry.toLoverCase();
+// if(destinationCountry.toLoverCase() === china.toLoverCase()){
+//     possibleCountry = china;
+//     price = 100;
+// } else if(destinationCountry.toLoverCase() === australia.toLoverCase()){
+//     possibleCountry = australia;
+//     price = 170;
+// }
+console.log(dc)
+console.log(price)
